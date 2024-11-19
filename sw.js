dd7c2f8f42466dce1471600ea193b0616e8cb3af
MandyHenderson <MandyHenderson@163.com>
2024-11-19 23:36:36 +0800

    Site updated: 2024-11-19 23:36:36

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732029218136";
+const VERSION = "1732030565013";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
